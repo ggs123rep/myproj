501faacf788bf93d062a2c96ba728226c0f19e4a
charansaigirish@gmail.com <charansaigirish@gmail.com>
2021-07-22 18:37:31 +0530

    done mama

--- EurekaClient1/src/main/java/dru/eureka/client/DemoApplication.java
+++ EurekaClient1/src/main/java/dru/eureka/client/DemoApplication.java
@@ -1,5 +1,5 @@
 package dru.eureka.client;
-//testetstetcxzcsdcsd
+//testetstet
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class DemoApplication {
-
+//main
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
